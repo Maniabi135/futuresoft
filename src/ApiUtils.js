bd4fdbe9dc8414d4b60e200c0499b8b89fe3c20a
Manikandan Subramaniyan <manikandan@adapt.io>
2021-05-27 23:34:19 +0530

    update

--- src/ApiUtils.js
+++ src/ApiUtils.js
@@ -15,12 +15,7 @@ export const makeApi = (config, doCancelRequest) => {
   config.headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": true,
-    "Access-Control-Allow-Headers": "X-Requested-With,content-type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   };
-  config.withCredentials = true;
   if (doCancelRequest !== undefined) {
     config.cancelToken = new CancelToken(function executor(cancelHttpRequest) {
       doCancelRequest(cancelHttpRequest);
